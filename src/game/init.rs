09bfceca0ab6985431c87a0589d2a99d3e27dbf4
amir <amirf3894@outlook.com>
2025-06-18 23:12:42 +0330

    cusror position added

--- src/game/init.rs
+++ src/game/init.rs
@@ -1,10 +1,13 @@
 use core::time;
 use crossterm::{
-    cursor,
-    event::{self, Event, KeyCode},
+    cursor::{self, MoveTo},
+    event::{self, Event::Key, KeyCode, KeyEvent, read},
     execute,
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+    terminal::{
+        self, EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode,
+    },
 };
+
 use std::{
     io::{self, Write, stdout},
     thread::sleep,
@@ -14,9 +17,38 @@ use std::{
 pub fn start() -> io::Result<()> {
     enable_raw_mode()?;
     let mut stdout = stdout();
-    execute!(stdout, EnterAlternateScreen)?;
-    sleep(Duration::from_secs(8));
-    execute!(stdout, LeaveAlternateScreen, cursor::Show)?;
+    execute!(stdout, EnterAlternateScreen, terminal::SetSize(256, 256))?;
+    let terminal_size = terminal::size()?;
+    let mut cursor_position = (terminal_size.0 / 2, terminal_size.1 / 2);
+    execute!(stdout, cursor::MoveTo(cursor_position.0, cursor_position.1))?;
+    loop {
+        let key_pressed = read()?;
+        if let Some(key) = key_pressed.as_key_event() {
+            match key.code {
+                KeyCode::Right => cursor_position.0 = cursor_position.0.saturating_add(1),
+                KeyCode::Left => cursor_position.0 = cursor_position.0.saturating_sub(1),
+                KeyCode::Up => cursor_position.1 = cursor_position.1.saturating_sub(1),
+                KeyCode::Down => cursor_position.1 = cursor_position.1.saturating_add(1),
+                _ => (print!("{:?},", key.code)),
+            }
+            let moved_curser = cursor::MoveTo(cursor_position.0, cursor_position.1);
+            execute!(stdout, moved_curser);
+        }
+    }
+    // lxecute!(stdout, EnterAlternateScreen,)?;
+    let size = terminal::size()?;
+    // for _ in 0..erminal::size()?;
+    // println!("{:?}", size);
+
+    sleep(Duration::from_secs(2));
+    execute!(stdout, LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
+
+pub fn end() -> io::Result<()> {
+    let mut stdout = stdout();
+    execute!(stdout, LeaveAlternateScreen)?;
     disable_raw_mode()?;
     Ok(())
 }

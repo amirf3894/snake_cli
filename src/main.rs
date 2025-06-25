86f18d46fc87a463b59388795dd38947cd4d411a
amir <amirf3894@outlook.com>
2025-06-25 13:21:37 +0330

    now clap added for cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
+use std::process::exit;
+
+use clap::{Arg, ArgMatches, Command};
 use snake::game::snake;
 
 #[tokio::main]
 async fn main() {
+    let matches = Command::new("snake")
+        .subcommand(
+            Command::new("host")
+                .arg(Arg::new("ip").short('i').long("ip").required(true))
+                .arg(Arg::new("size").short('s').long("size")),
+        )
+        .subcommand(
+            Command::new("client")
+                .arg(Arg::new("ip").short('i').long("ip").required(true))
+                .arg(Arg::new("name").required(true)),
+        )
+        .subcommand(Command::new("solo"))
+        .subcommand_required(true)
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("host", arg)) => host(arg),
+        Some(("client", arg)) => client(arg),
+        Some(("solo", _)) => solo(),
+        _ => exit(0),
+    }
+
+    fn host(arg: &ArgMatches) {}
+    fn client(arg: &ArgMatches) {}
+    fn solo() {}
     snake::main_snake().await.unwrap();
 }
